f2159506f74bca56fafbf7604775e79dc1aa653f
Renan Santos <renan-bs@hotmail.com>
2024-04-27 12:26:42 -0300

    feat: Gerando dados fictícios com faker-js

--- test/factories/make-exercise.ts
+++ test/factories/make-exercise.ts
@@ -1,16 +1,24 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Exercise,
   ExerciseProps,
 } from '@/domain/task/enterprise/entities/exercise'
 
-export function makeExercise(override: Partial<ExerciseProps> = {}) {
-  const exercise = Exercise.create({
-    title: 'Example Exercise',
-    authorId: new UniqueEntityID(),
-    content: 'Exercise content',
-    ...override,
-  })
+export function makeExercise(
+  override: Partial<ExerciseProps> = {},
+  id?: UniqueEntityID,
+) {
+  const exercise = Exercise.create(
+    {
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return exercise
 }
